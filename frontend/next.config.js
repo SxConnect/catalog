79cf72208d03f6fbef70cf033d7decb66e19df88
SxConnect <dev@sxconnect.com>
2026-03-09 08:06:06 -0300

    docs: Adiciona diagnóstico completo e solução final

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,9 +4,6 @@ const nextConfig = {
     images: {
         domains: ['mins3.sxconnect.com.br', 'localhost'],
     },
-    experimental: {
-        serverActions: false,
-    },
 }
 
 module.exports = nextConfig
